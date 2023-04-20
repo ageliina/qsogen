3494fa8aae1132ef41040cdde080f12a5f504adb
Akke Viitanen <akke.viitanen@helsinki.fi>
2023-04-20 15:56:22 +0200

    Fixes config path

--- config.py
+++ config.py
@@ -14,20 +14,22 @@ Edit 2022 May: update reference to published paper 2021MNRAS.508..737T
 """
 import numpy as np
 
-f1 = 'qsosed_emlines_20210625.dat'
+PATH_TO_QSOGEN = "/home/aetviita/Sync/Projects/inaf/lsst/lsst_galaxy_agn_mock/qsogen"
+
+f1 = f'{PATH_TO_QSOGEN}/qsosed_emlines_20210625.dat'
 emline_template = np.genfromtxt(f1, unpack=True)
 # wav, median_emlines, continuum, peaky_line, windy_lines, narrow_lines
 
-f2 = 'S0_template_norm.sed'
+f2 = f'{PATH_TO_QSOGEN}/S0_template_norm.sed'
 galaxy_template = np.genfromtxt(f2, unpack=True)
 # S0 galaxy template from SWIRE
 # https://ui.adsabs.harvard.edu/abs/2008MNRAS.386..697R/
 
-f3 = 'pl_ext_comp_03.sph'
+f3 = f'{PATH_TO_QSOGEN}/pl_ext_comp_03.sph'
 reddening_curve = np.genfromtxt(f3, unpack=True)
 # Extinction curve, format: [lambda, E(lambda-V)/E(B-V)]
 # Recall flux_reddened(lambda) = flux(lambda)*10^(-A(lambda)/2.5)
-# where A(lambda) = E(B-V)*[E(lambda-V)/E(B-V) + R] 
+# where A(lambda) = E(B-V)*[E(lambda-V)/E(B-V) + R]
 # so taking R=3.1, A(lambda) = E(B-V)*[Col#2 + 3.1]
 
 # fit to DR16Q median 2sigma-clipped colours in multi-imag bins
